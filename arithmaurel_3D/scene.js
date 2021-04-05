4a23dc870017893d020821150b5a124c495c0b54
Nicky Tching <nicky.tching@grenoble-inp.org>
2021-04-05 13:25:22 +0200

    ajout boitier a trou

--- arithmaurel_3D/scene.js
+++ arithmaurel_3D/scene.js
@@ -35,7 +35,7 @@ function init() {
    // instantiate a loader 
    var loader = new ColladaLoader();
    // instancie une roue
-   loader.load( 'modeles_3D/boitier2.dae',
+   loader.load( 'modeles_3D/test.dae',
 
        // Function when resource is loaded
        function (collada) {
